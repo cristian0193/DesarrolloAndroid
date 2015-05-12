99e8f8387b8d201a39fe377d8df9170bd8c867d9
cristian0193 <cristian010193@hotmail.com>
2015-05-12 15:43:09 -0500

    EjemploAndroid
    Permite mediante un boton cambiar el color del Layout

---
+++ MyApplication/app/src/main/java/com/example/cristian/myapplication/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.cristian.myapplication;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    RelativeLayout background;
+    Button btnVerde;
+    Button btnAzul;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        background = (RelativeLayout) findViewById(R.id.background);
+        btnAzul = (Button) findViewById(R.id.btnAzul);
+        btnVerde = (Button) findViewById(R.id.btnVerde);
+
+        btnVerde.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            background.setBackgroundColor(Color.parseColor("#00ff00"));
+
+                                        }
+                                    }
+        );
+
+        btnAzul.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            background.setBackgroundColor(Color.parseColor("#006699"));
+                                        }
+                                    }
+
+        );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
